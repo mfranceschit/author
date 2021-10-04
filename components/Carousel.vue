fb314bdb295d2198b4636e629a9ec19d5ce77d1e
Marco Franceschi <franceschi.marco@gmail.com>
2021-10-04 11:18:27 -0300

    Initial commit: Nuxt configuration

---
+++ components/Carousel.vue
@@ -0,0 +1,31 @@
+<template>
+  <carousel-3d
+    :controls-visible="true"
+    :clickable="true"
+    :width="500"
+    :height="600"
+    :perspective="0"
+  >
+    <slide :index="0"> <HardCoverBook /> </slide>
+    <slide :index="1"><HardCoverBook /> </slide>
+    <slide :index="2"> <HardCoverBook /> </slide>
+  </carousel-3d>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
+
+export default {
+  components: {
+    Carousel3d,
+    Slide,
+  },
+}
+</script>
+
+<style>
+.carousel-3d-container .carousel-3d-slide {
+  background-color: transparent;
+  border: none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Carousel />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+#__layout {
+  display: flex;
+  height: 100vh;
+  align-items: center;
+}
+</style>
